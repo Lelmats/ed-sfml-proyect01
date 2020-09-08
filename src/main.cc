20f296072084cee4babb96ce413350b86645b7f5
Lelmats <maxalejandrorh@hotmail.com>
2020-09-08 09:05:56 -0700

    primer commit

---
+++ src/main.cc
@@ -0,0 +1,11 @@
+#include<iostream>
+#include <SFML/Graphics.hpp>
+int main()
+{
+  sf::RenderWindow window(sf::VideoMode(800, 600), "Platform Game");
+    sf::Event event;
+    while(window.isOpen())
+    {
+       
+    }
+}
