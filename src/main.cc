211999b77189cbcd0c7666b1af57f60019399e1d
Lelmats <maxalejandrorh@hotmail.com>
2020-09-14 03:20:21 -0700

    Texture

--- src/main.cc
+++ src/main.cc
@@ -1,11 +1,59 @@
 #include<iostream>
 #include <SFML/Graphics.hpp>
+
+#include "Inputs.hh"
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+#define GAME_NAME "Roguelike game"
+
 int main()
 {
-  sf::RenderWindow window(sf::VideoMode(800, 600), "Platform Game");
-    sf::Event event;
-    while(window.isOpen())
+    //esto es la ventana de tu grafico
+    sf::RenderWindow* window = new sf::RenderWindow(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), GAME_NAME);
+    //aqui vas a guardar los eventos dentro de la ventana, eje: teclado, mouse, etc.
+    Inputs* inputs = new Inputs();
+   
+    sf::Texture texture;
+
+    if (!texture.loadFromFile("C:/Users/maxal/Documents/res/TrainS/z.png"))
     {
-       
+        std::cout << "Load failed" << std::endl;
+
+        system("pause");
     }
+
+    sf::Sprite sprite;
+    sprite.setTexture(texture);
+
+    //esto es el loop principal, mientras la ventana este abierta, esto se va ejecutar.
+    while (window->isOpen())
+    {
+        sf::Event event;
+        //mientras se esten ejecutando eventos dentro de la ventana, esto se va repetir eje: teclado, joystick, mouse, etc
+        while (window->pollEvent(event))
+        {
+            //si el evento fue la acción de cerrar la ventana, entonces termina la aplicación.
+            if(event.type == sf::Event::Closed)
+            {
+                window->close();
+                
+            }
+        }
+        window->clear();
+        window->draw(sprite);
+        window->display();
+        Vec2* keyboardAxis{inputs->GetKeyboardAxis()};
+        Vec2* joystickAxis{inputs->GetJoystickAxis()};
+        //std::cout << "keyboard axis x: " << keyboardAxis->x << " keyboard axis y: " << keyboardAxis->y << std::endl;
+        
+        std::cout << "joystic axis x: " << joystickAxis->x << " joystic axis y: " << joystickAxis->y << std::endl;
+
+        //inputs->GetJoystickAxis();
+        delete keyboardAxis;
+        delete joystickAxis;
+        
+    }
+    
+    return 0;
 }
